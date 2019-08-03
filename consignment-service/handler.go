31350ad448794bf139ee347ae0d0b4580ef25bfc
sandeepsambidi <sandeep.sambidi@gmail.com>
2019-08-03 17:47:39 +0530

    added docker-compose file, separated consignment service handler to a new handler file

---
+++ consignment-service/handler.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"context"
+	"log"
+
+	pbcon "github.com/sandeepsambidi/ekart/consignment-service/proto/consignment"
+	pbves "github.com/sandeepsambidi/ekart/vessel-service/proto/vessel"
+)
+
+// Service should implement all of the methods to satisfy the service
+// we defined in our protobuf definition. You can check the interface
+// in the generated code itself for the exact method signatures etc
+// to give you a better idea.
+type service struct {
+	repo         repository
+	vesselClient pbves.VesselService
+}
+
+// CreateConsignment - we created just one method on our service,
+// which is a create method, which takes a context and a request as an
+// argument, these are handled by the gRPC server.
+func (s *service) CreateConsignment(con context.Context, req *pbcon.Consignment, res *pbcon.Response) error {
+	//1. find a vessel for the consignment
+	//2. attach vessel to the consignment
+	//3. store the consignment in the DB
+	specification := &pbves.Specification{
+		MaxWeight: req.GetWeight(),
+		Capacity:  int32(len(req.Containers))}
+	vesselResponse, err := s.vesselClient.FindAvailable(context.Background(), specification)
+
+	if err != nil {
+		return err
+	}
+
+	log.Printf("Found vessel: %s", vesselResponse.Vessel.GetName())
+
+	req.VesselId = vesselResponse.Vessel.GetId()
+
+	//create the consignment ie add a document in consignment collection
+	createdcon, err := s.repo.Create(req)
+
+	if err != nil {
+		return err
+	}
+
+	res.Created = true
+	res.Consignment = createdcon
+	return nil
+}
+
+//get all consignments
+func (s *service) GetConsignments(con context.Context, req *pbcon.GetRequest, res *pbcon.Response) error {
+	consignments := s.repo.GetAll()
+	res.Consignments = consignments
+	return nil
+}
--- consignment-service/main.go
+++ consignment-service/main.go
@@ -2,10 +2,8 @@ package main
 
 import (
 	"fmt"
-	"log"
 
 	// Import the generated protobuf code
-	"context"
 
 	micro "github.com/micro/go-micro"
 
@@ -34,52 +32,6 @@ func (repo *Repository) Create(consignment *pb.Consignment) (*pb.Consignment, er
 // GetAll consignments
 func (repo *Repository) GetAll() []*pb.Consignment {
 	return repo.consignments
-}
-
-// Service should implement all of the methods to satisfy the service
-// we defined in our protobuf definition. You can check the interface
-// in the generated code itself for the exact method signatures etc
-// to give you a better idea.
-type service struct {
-	repo         repository
-	vesselClient pbvessel.VesselService
-}
-
-// CreateConsignment - we created just one method on our service,
-// which is a create method, which takes a context and a request as an
-// argument, these are handled by the gRPC server.
-func (s *service) CreateConsignment(ctx context.Context, req *pb.Consignment, res *pb.Response) error {
-
-	vesselResponse, err := s.vesselClient.FindAvailable(ctx,
-		&pbvessel.Specification{
-			MaxWeight: req.Weight,
-			Capacity:  int32(len(req.Containers))})
-
-	log.Printf("Found vessel: %s \n", vesselResponse.Vessel.Name)
-	if err != nil {
-		return err
-	}
-
-	req.VesselId = vesselResponse.Vessel.Id
-
-	// Save our consignment
-	consignment, err := s.repo.Create(req)
-	if err != nil {
-		return err
-	}
-
-	// Return matching the `Response` message we created in our
-	// protobuf definition.
-	res.Created = true
-	res.Consignment = consignment
-	return nil
-}
-
-// GetConsignments -
-func (s *service) GetConsignments(ctx context.Context, req *pb.GetRequest, res *pb.Response) error {
-	consignments := s.repo.GetAll()
-	res.Consignments = consignments
-	return nil
 }
 
 func main() {
